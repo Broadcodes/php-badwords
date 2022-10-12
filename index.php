8c5428cf0c50f4943130fd336a24c223d0d789fb
GianlucaFabrizio <gianlucafabrizio@hotmil.com>
2022-10-12 15:52:33 +0200

    Implementazione dell'articolo censurato

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 
  <?php
 
+    $censure = $_GET['element'];
+ 
     $article = 'Passava quasi tutte le notti alla finestra a spazzolarsi una mano a quei duri gremiti suoi capelli da negro, con
     gli occhi alle stelle, placide e chiare le une come polle di luce, guizzanti e pungenti le altre; e metteva le più
     vive in rapporti ideali di figure geometriche, di triangoli e di quadrati, e, socchiudendo le palpebre dietro le
@@ -17,10 +19,14 @@
     produce quasi sempre, specie a certuni che hanno in sè una certezza su la quale non possono riposare, la
     certezza di non poter nulla sapere e nulla credere non sapendo, qualche seria costipazione. Costipazione
     d’anima, s’intende. E al giudice D’Andrea, quando si faceva giorno, pareva una cosa buffa e atroce nello
-    stesso tempo, ch’egli dovesse recarsi al suo ufficio d’Istruzione ad amministrare – per quel tanto che a lui
-    toccava – la giustizia ai piccoli poveri uomini feroci.'
+    stesso tempo, ch’egli dovesse recarsi al suo ufficio d’Istruzione ad amministrare per quel tanto che a lui
+    toccava la giustizia ai piccoli poveri uomini feroci.'
  ?>
 
  <?= '<h2>Articolo originale:</h2>' ?>
  <?= $article?>
  <?= '<h3>Lunghezza articolo: ' . strlen($article) . ' caratteri</h3>'?>
+
+ <?= '<h2>Articolo censurato:</h2>' ?>
+ <?= $censoredArticle = str_replace($censure, '***', $article)?>
+ <?= '<h3>Lunghezza articolo: ' . strlen($censoredArticle) . ' caratteri</h3>'?>
